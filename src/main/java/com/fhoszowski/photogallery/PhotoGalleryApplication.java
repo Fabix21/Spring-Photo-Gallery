cee76553808185d1a2b500c5a6d57940001dd988
Fabian Hoszowski <fabix43@gmail.com>
2020-08-11 04:30:12 +0200

    Changed media path to match new endpoint.

--- src/main/java/com/fhoszowski/photogallery/PhotoGalleryApplication.java
+++ src/main/java/com/fhoszowski/photogallery/PhotoGalleryApplication.java
@@ -1,15 +1,13 @@
 package com.fhoszowski.photogallery;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.io.IOException;
 
 @SpringBootApplication
 public class PhotoGalleryApplication implements WebMvcConfigurer {
-    public static void main( String[] args ) throws IOException {
+    public static void main( String[] args ) {
         SpringApplication.run(PhotoGalleryApplication.class,args);
 
     }
@@ -17,10 +15,7 @@ public class PhotoGalleryApplication implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers( ResourceHandlerRegistry registry ) {
 
-        // Register resource handler for images
-        // registry.addResourceHandler("/images/**").addResourceLocations("/WEB-INF/images/");
-        registry.addResourceHandler("/media/**").addResourceLocations("file:media/");
-
+        registry.addResourceHandler("gallery/media/**").addResourceLocations("file:media/");
     }
 
 }
